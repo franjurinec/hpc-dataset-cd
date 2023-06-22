ba6410adea675cd19f4f44e9aead1e630daa9ea0
Fran Jurinec <franjurinec@live.com>
2023-06-22 17:47:33 +0000

    Update

--- .github/workflows/slurm-execute.sh
+++ .github/workflows/slurm-execute.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 TAG_SLUG=$(echo $TAG | iconv -t ascii//TRANSLIT | sed -E -e 's/[^[:alnum:]]+/-/g' -e 's/^-+|-+$//g' | tr '[:upper:]' '[:lower:]')
-# git clone --depth 1 --branch $TAG $REPO_URL $TARGET_TMP_DIR/$TAG_SLUG
-# cd $TARGET_TMP_DIR/$TAG_SLUG
+git clone --depth 1 --branch $TAG $REPO_URL $TARGET_TMP_DIR/$TAG_SLUG
+cd $TARGET_TMP_DIR/$TAG_SLUG
 
 # rm -r $TARGET_TMP_DIR/$TAG_SLUG
-
-echo $TARGET_TMP_DIR/$TAG_SLUG
-echo $TAG
-echo $REPO_URL
---
+++ workflow.py
@@ -0,0 +1,5 @@
+"""Module providing sleep function."""
+import time
+
+time.sleep(5)
+print('Hello world!')
