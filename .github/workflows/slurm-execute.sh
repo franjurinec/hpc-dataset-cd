6ed7a5c4200cd9a6b8a435c756088cc083ca929b
Fran Jurinec <franjurinec@live.com>
2023-06-22 18:01:39 +0000

    Update

--- .github/workflows/slurm-execute.sh
+++ .github/workflows/slurm-execute.sh
@@ -3,4 +3,6 @@ TAG_SLUG=$(echo $TAG | iconv -t ascii//TRANSLIT | sed -E -e 's/[^[:alnum:]]+/-/g
 git clone --depth 1 --branch $TAG https://github.com/$REPO.git $TARGET_TMP_DIR/$TAG_SLUG
 cd $TARGET_TMP_DIR/$TAG_SLUG
 
+# EXECUTE sbatch
+
 rm -r $TARGET_TMP_DIR/$TAG_SLUG -y
