4ef72288db28bbb90cff32cfbf4420a9d975cf1b
Fran Jurinec <franjurinec@live.com>
2023-07-02 20:16:59 +0000

    Metadata Output

---
+++ data_helper.py
@@ -0,0 +1,62 @@
+import os
+import json
+from random import randint
+import s3fs
+import numpy as np
+from dotenv import load_dotenv
+
+# Load .env variables
+load_dotenv()
+
+# Data Input Metadata
+in_meta = {
+    "source_facility": "Source Facility",
+    "pulses": {}
+}
+
+# Typically this would be a facility-specific access method
+remote_fs = s3fs.S3FileSystem(
+    key=os.getenv('S3_KEY'),
+    secret=os.getenv('S3_PASS'),
+    client_kwargs={
+        'endpoint_url': 'https://a3s.fi'
+    }
+)
+
+def get_pulse(shot_number, diagnostic_signal_dict):
+    pulse_dict = {}
+    for diagnostic, signal_names in diagnostic_signal_dict.items():
+        for signal_name in signal_names:
+            signal_dict = {}
+            for dimension in ['data', 'time']:
+                object_name =  f'{shot_number}_{diagnostic}_{signal_name}_{dimension}.npy'
+                try:
+                    data = np.load(remote_fs.open(f'JET_PULSES/{object_name}'), allow_pickle=True)
+                    if diagnostic in ['efit', 'power', 'magn', 'gas']: 
+                        data = data.item()[dimension]
+                    if isinstance(data, str) and signal_name == 'PICR_TOT': 
+                        signal_dict['time'] = pulse_dict['PNBI_TOT']['time']
+                        signal_dict['data'] = np.zeros_like(pulse_dict['PNBI_TOT']['time'])
+                    else:
+                        signal_dict[dimension] = data
+                except FileNotFoundError as _:
+                    print(f'Did not find {object_name} in JET_PULSES, not returning Pulse')
+                    in_meta["pulses"][shot_number] = {
+                            'pulse': shot_number, 
+                            'success': False
+                            }
+                    return None
+            pulse_dict[signal_name] = signal_dict
+    in_meta["pulses"][shot_number] = {
+        'pulse': shot_number, 
+        'success': True, 
+        'diagnostics': {k:{'signals': v, 'calibration': randint(1,10)} for k, v in diagnostic_signal_dict.items()} 
+        }
+    return pulse_dict
+
+def output_metadata(base_metadata, out_dir):
+    metadata = base_metadata
+    metadata["source"] = in_meta
+    with open(os.path.join(out_dir, 'tmp/meta', 'metadata.json'), "w", encoding="UTF-8") as meta_file:
+        meta_json_object = json.dumps(metadata, indent=4)
+        meta_file.write(meta_json_object)
--- workflow.py
+++ workflow.py
@@ -1,12 +1,13 @@
 import os
-import s3fs
 from dotenv import load_dotenv
 import numpy as np
+from data_helper import get_pulse, output_metadata
 from data_transform import convert_pulse_dict_to_numpy_array
 
 # Load .env variables
 load_dotenv()
 
+# Define basic inputs and metadata
 PULSE_LIST = [81768,81798,85306,92207, 95479]
 SIGNAL_DICT = {
     'efit': ['Rgeo', 'ahor', 'Vol', 'delRoben', 'delRuntn', 'k'],
@@ -15,41 +16,21 @@ SIGNAL_DICT = {
     'gas': ['D_tot'],
     'hrts': ['radius', 'ne', 'ne_unc', 'Te', 'Te_unc']  
 }
-METADATA = {}
-BASE_DIR = os.getcwd()
+METADATA = {
+    "title": "Demo Dataset",
+    "description": "Dataset used to demo a new research data workflow approach.",
+    "authors": [
+        {"name": "Adam", "surname": "Kit"}
+    ],
+}
 
-# Typically this would be a facility-specific access method
-remote_fs = s3fs.S3FileSystem(
-    key=os.getenv('S3_KEY'),
-    secret=os.getenv('S3_PASS'),
-    client_kwargs={
-        'endpoint_url': 'https://a3s.fi'
-    }
-)
+# Prepare otuput directories for data and metadata
+DATA_DIR = os.path.join(os.getcwd(), 'tmp/out')
+os.makedirs(DATA_DIR, exist_ok=True)
+META_DIR = os.path.join(os.getcwd(), 'tmp/meta')
+os.makedirs(META_DIR, exist_ok=True)
 
-def get_pulse(shot_number, diagnostic_signal_dict):
-    pulse_dict = {}
-    for diagnostic, signal_names in diagnostic_signal_dict.items():
-        for signal_name in signal_names:
-            signal_dict = {}
-            for dimension in ['data', 'time']:
-                object_name =  f'{shot_number}_{diagnostic}_{signal_name}_{dimension}.npy'
-                try:
-                    data = np.load(remote_fs.open(f'JET_PULSES/{object_name}'), allow_pickle=True)
-                    if diagnostic in ['efit', 'power', 'magn', 'gas']: 
-                        data = data.item()[dimension]
-                    if isinstance(data, str) and signal_name == 'PICR_TOT': 
-                        signal_dict['time'] = pulse_dict['PNBI_TOT']['time']
-                        signal_dict['data'] = np.zeros_like(pulse_dict['PNBI_TOT']['time'])
-                    else:
-                        signal_dict[dimension] = data
-                except FileNotFoundError as _:
-                    print(f'Did not find {object_name} in JET_PULSES, not returning Pulse')
-                    return None
-            pulse_dict[signal_name] = signal_dict
-    return pulse_dict
-
-os.makedirs(os.path.join(BASE_DIR, 'tmp/out'), exist_ok=True)
+# Execute data tranformations
 for pulse_id in PULSE_LIST:
     pulse = get_pulse(pulse_id, SIGNAL_DICT)
     if pulse is None:
@@ -57,6 +38,9 @@ for pulse_id in PULSE_LIST:
         continue
     pulse_transformed = convert_pulse_dict_to_numpy_array(pulse)
     for key, value in pulse_transformed.items():
-        result_path = os.path.join(BASE_DIR, 'tmp/out', f'{pulse_id}_{key}.npy')
+        result_path = os.path.join(DATA_DIR, f'{pulse_id}_{key}.npy')
         print(f'Saving {result_path}')
         np.save(result_path, value)
+
+# Output metadata.json to metadata directory
+output_metadata(METADATA, META_DIR)
